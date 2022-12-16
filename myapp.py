e0df84e540d6eb66e7f09fc2a053a1c40280edb7
Stanislav Kukucka <89223349+stanislav-kukucka@users.noreply.github.com>
2022-12-16 12:03:20 +0100

    Add files via upload

--- myapp.py
+++ myapp.py
@@ -8,7 +8,8 @@ st.title("URL grabber from sitemap file")
 st.markdown("This is a tool for iFOCUS.sk Link Nest plugin - Internal linking for Wordpress")
 
 # use the suppress_st_warning parameter to suppress the warning message
-@st.cache(suppress_st_warning=True)  
+@st.cache(suppress_st_warning=True)
+
 def sitemap_urls(url):
     with st.spinner("Wait for it..."):
         time.sleep(10)
@@ -21,22 +22,23 @@ def sitemap_urls(url):
         # find the sitemap tree for the homepage
         tree = sitemap_tree_for_homepage(url)
         st.success("Done!")
-
+        
     urls = []
 
     for page in tree.all_pages():
         page = page.url
         urls.append(page)
-
+        
     return urls
-
-x = st.text_input("Insert Your URL. We will find your sitemap file. It will take few seconds.")
+    
+x = st.text_input("Insert Your URL. Example: yourdomain.com We will find your sitemap file. It will take few seconds.")
 
 if x:
     try:
         urls = sitemap_urls(x)
         df = pd.DataFrame({"Keyword": "somekeyword", "Atribute (title)": "somekeyword", "Atribute (rel)": "search", "URL": urls})
         st.download_button("Download URLs as CSV", df.to_csv(index=False), file_name="sitemap-urls.csv",)
+        
     except Exception as e:
         st.write("An error occurred:", e)
 else:
