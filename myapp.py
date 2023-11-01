079a49d3be2d97d6d427ce088857494f1f938f4a
Hamza Jadun <81479337+HamzaJadun@users.noreply.github.com>
2023-11-02 00:06:45 +0500

    app.py

--- myapp.py
+++ myapp.py
@@ -6,10 +6,12 @@ from urllib.parse import urlparse
 from usp.tree import sitemap_tree_for_homepage
 
 """
-# URL grabber from sitemap file
-This is a tool for [iFOCUS.sk](https://www.ifocus.sk) Link Nest plugin - Internal linking for Wordpress
+# Sitemap URL Extractor
 
-We will find your sitemap file. It will take few seconds.
+Welcome to the Sitemap URL Extractor, your one-stop tool for discovering and extracting URLs from sitemaps!
+
+This interactive Streamlit app was crafted by [Your Name] to simplify the process of finding and exporting URLs from sitemaps. Just enter a website URL, click the "Search" button, and let the magic happen!
+
 """
 
 st.markdown('''
@@ -22,44 +24,45 @@ st.markdown('''
 
 @st.cache(suppress_st_warning=True, allow_output_mutation=True)
 def sitemap_urls(url):
-    with st.spinner("Wait for it..."):
+    with st.spinner("Scanning for sitemap..."):
         time.sleep(10)
 
-        # parse the URL to ensure it is a valid HTTP(S) URL
+        # Ensure the URL is valid with an HTTP(S) scheme
         parsed_url = urlparse(url)
         if not parsed_url.scheme:
             url = "https://" + url
 
-        # find the sitemap tree for the homepage
+        # Find the sitemap tree for the homepage
         tree = sitemap_tree_for_homepage(url)
-        st.success("Done!")
-        
+        st.success("Sitemap detected!")
+
     urls = []
 
     for page in tree.all_pages():
         page = page.url
         urls.append(page)
-        
+
     return urls
-    
-x = st.text_input("")
 
-if x:
-    # Check if the URL has a scheme (http or https)
-    if not urlparse(x).scheme:
-        # If the URL doesn't have a scheme, add 'http://' to the beginning
-        x = 'http://' + x
-        
-    # Check if the URL responds with a status code of 200
-    try:
-        if requests.get(x).status_code == 200:
-            urls = sitemap_urls(x)
-            df = pd.DataFrame({"Keyword": "somekeyword", "Atribute (title)": "somekeyword", "Atribute (rel)": "search", "URL": urls})
-            st.download_button("Download URLs as CSV", df.to_csv(index=False), file_name="sitemap-urls.csv",)
+x = st.text_input("Enter a website URL")
 
-        else:
-            st.error("ERROR: Please enter a valid URL that responds with a status code of 200.")
-    except Exception as e:
-        st.error("An error occurred: " + str(e))
-else:
-    st.write("INFO: Please enter a valid URL. (Example: yourdomain.com)")
+if st.button("Search"):
+    if x:
+        # Check if the URL has a scheme (http or https)
+        if not urlparse(x).scheme:
+            # If the URL doesn't have a scheme, add 'http://' to the beginning
+            x = 'http://' + x
+
+        # Check if the URL responds with a status code of 200
+        try:
+            if requests.get(x).status_code == 200:
+                urls = sitemap_urls(x)
+                df = pd.DataFrame({"Keyword": "SEO", "Attribute (title)": "Description", "Attribute (rel)": "Follow", "URL": urls})
+                st.download_button("Download Extracted URLs as CSV", df.to_csv(index=False), file_name="sitemap-urls.csv",)
+
+            else:
+                st.error("ERROR: Please enter a valid URL that responds with a status code of 200.")
+        except Exception as e:
+            st.error("An error occurred: " + str(e))
+    else:
+        st.warning("Please enter a website URL before clicking the 'Search' button.")
